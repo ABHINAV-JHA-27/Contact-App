fca2a6baf4c455bd0f5f51e7ab385fc6acd20b86
ABHINAV JHA <75615092+ABHINAV-JHA-27@users.noreply.github.com>
2023-05-06 19:35:58 +0530

    Added Navigation

--- src/Navigation/index.js
+++ src/Navigation/index.js
@@ -1,10 +1,58 @@
-import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import * as React from "react";
+import AddEditNoteScreen from "../Screens/AddEditNoteScreen";
+import HomeScreen from "../Screens/HomeScreen";
+import SplashScreen from "../Screens/SplashScreen";
+import ViewNoteScreen from "../Screens/ViewNoteScreen";
+
+const Stack = createNativeStackNavigator();
+
+const horizontalAnimation = {
+    headerShown: false,
+    gestureDirection: "horizontal",
+    cardStyleInterpolator: ({ current, layouts }) => {
+        return {
+            cardStyle: {
+                transform: [
+                    {
+                        translateX: current.progress.interpolate({
+                            inputRange: [0, 1],
+                            outputRange: [layouts.screen.width, 0],
+                        }),
+                    },
+                ],
+            },
+        };
+    },
+};
 
 const index = () => {
     return (
-        <View>
-            <Text>index</Text>
-        </View>
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Splash">
+                <Stack.Screen
+                    name="Splash"
+                    component={SplashScreen}
+                    options={horizontalAnimation}
+                />
+                <Stack.Screen
+                    name="Home"
+                    component={HomeScreen}
+                    options={horizontalAnimation}
+                />
+                <Stack.Screen
+                    name="AddNote"
+                    component={AddEditNoteScreen}
+                    options={horizontalAnimation}
+                />
+                <Stack.Screen
+                    name="ViewNote"
+                    component={ViewNoteScreen}
+                    options={horizontalAnimation}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
     );
 };
 
